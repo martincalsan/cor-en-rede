c7de3a6dfca6bb3adc685f01ddd504d7ade5e1e1
martincalsan <lordtheiax@gmail.com>
2026-05-22 02:18:09 +0200

    v2.0 maximo 6 jugadores

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -8,7 +8,19 @@ public class HelloWorldManager : MonoBehaviour
         if (NetworkManager.Singleton.IsClient || NetworkManager.Singleton.IsServer)
             return;
 
-        if (GUILayout.Button("Host"))   NetworkManager.Singleton.StartHost();
+        if (GUILayout.Button("Host"))
+        {
+            NetworkManager.Singleton.ConnectionApprovalCallback = ApproveConnection;
+            NetworkManager.Singleton.StartHost();
+        }
         if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+    }
+
+    void ApproveConnection(
+        NetworkManager.ConnectionApprovalRequest req,
+        NetworkManager.ConnectionApprovalResponse res)
+    {
+        res.Approved = NetworkManager.Singleton.ConnectedClientsIds.Count < 6;
+        res.CreatePlayerObject = res.Approved;
     }
 }
